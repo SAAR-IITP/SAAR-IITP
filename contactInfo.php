41517037065beeec50b764140bd5ae2056ceafb3
VatsalSin <vatsal.eliot@gmail.com>
2018-07-24 00:42:02 +0530

    Fixed bugs in contactInfo.php

--- contactInfo.php
+++ contactInfo.php
@@ -9,29 +9,6 @@
 	//echo $registered." row is affected, everything worked fine!";
 	if($registered==1)
 	{
-		echo "Registration Complete";
+		echo "THANK YOU PAGE WILL OPEN";
 	}
-	require 'PHPMailer\PHPMailer.php';
-$mail = new PHPMailer(); // create a new object
-$mail->IsSMTP(); // enable SMTP
-$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-$mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-$mail->Host = "smtp.gmail.com";
-$mail->Port = 465; // or 587
-$mail->IsHTML(true);
-$mail->Username = "vatsal.eliot@gmail.com";
-$mail->Password = "cartoon11";
-$mail->SetFrom("example@gmail.com");
-$mail->Subject = "Test";
-$mail->Body = "hello";
-$mail->AddAddress($email);
-
- if(!$mail->Send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
- } else {
-    echo "Message has been sent";
- }
-				$mail->Body    = "Hello ".$name;
-
 ?>
