a72133e9e056c4d049ad2609faa5d7691d27a0f2
vatsalsin <vatsal.eliot@gmail.com>
2018-09-20 16:32:45 +0530

    minor changes in index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start();
         <title>Saar! Homepage</title>
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="asset/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
         <link href="style.css" rel="stylesheet" type="text/css"/>
         <link href="stylesheet/index/974.less" rel="stylesheet" type="text/less" media="only screen and (max-width:974px)"/>
@@ -17,7 +17,7 @@ session_start();
         <link href="stylesheet/index/577.less" rel="stylesheet" type="text/less" media="only screen and (max-width:577px)"/>
         <link href="stylesheet/index/458.less" rel="stylesheet" type="text/less" media="only screen and (max-width:458px)"/>
         <script src="asset/less js/less.min.js" type="text/javascript"></script>
-
+        
     </head>
     <body style="font-family: 'Open Sans', sans-serif">
         <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-color fixed-top pad-5">
@@ -31,7 +31,7 @@ session_start();
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto navbar-right">
               <li class="nav-item">
-              <a class="nav-link active active1" href="#">Home</a>
+                  <a class="nav-link active active1" href="#">Home</a>
             </li>
 
             <li class="nav-item">
@@ -100,7 +100,7 @@ session_start();
                         <div class="col-6 img">
                             <center><img src="asset/img/contact.png">
                             
-                            <a href="">CONTACT US</a></center>
+                                <a href="" onclick="return contactUs()">CONTACT US</a></center>
                         </div>
 
                     </div>
@@ -112,7 +112,7 @@ session_start();
                         </div>
                         <div class="col-6 img">
                             <center><img src="asset/img/help-icon.png">
-                            <a href="">NEED HELP</a> </center>
+                                <a href="" onclick="return show_pop()">HELP US GROW</a> </center>
                         </div>
 
                     </div>
@@ -136,7 +136,12 @@ session_start();
                             <button class="submit" type="submit" name="submit">Submit</button>
                             </form>
                         </div>
-                        <div class="div-right"><h1>SignUp Here</h1><br><h4>If you don't have an account.<br><button class="submit pad-2"><a href='signup.php?type=normal' id='sign-up'>SignUp</a></button></h4></div>
+                        <div class="div-right"><h1>SignUp Here</h1><br>
+                            <h4>If you don't have an account.<br>
+                                <button class="submit pad-2"><a href='signup.php?type=normal' id='sign-up'>SignUp</a></button><br>
+                                <button class="submit pad-2" style="background-color:#3B579D; background-image: none;"><a href='signup.php?type=normal' id='sign-up' style="color:#fff !important;font-size: 15px;">SignUp with Facebook</a></button>
+                            </h4>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -177,7 +182,7 @@ session_start();
                         <center><span><img src="https://www.biography.com/.image/t_share/MTE5NTU2MzE2MTg0NzQxMzg3/winston-churchill-9248164-1-402.jpg" alt="jay asher"></span></center>
                         <center><span>Winston Churchill</span></center>
                     </div>
-                    <div class="donate-btn"><div class="donate-btn-c" ><a href="donateUs/">Donate Now</a></div></div>
+                    <div class="donate-btn"><div class="donate-btn-c" ><a href="donateUs.php">Donate Now</a></div></div>
                 </div>
             </div>
                 </div>
@@ -333,13 +338,32 @@ session_start();
                                         <div class="icon-text">get response from alumni</div>
                                     </div>
                                 </div>
-                                <div class="post-need"><button>POST A NEED</button></div>
+                                <div class="post-need"><button onclick="show_form()">LOGIN/SIGNUP</button></div>
                             </center>
                         </div>
                     </div>
                 </div>
                 
                 <div class="col-3"></div>
+            </div>
+        </div>
+        <div class=" contact-form" id="contact_form" style="display:none;">
+            <div class="myclose" id="close" onclick="close_modal()">&times;</div>
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="jumbotron">
+                        <h5>STUDENT ASSOCIATION FOR ALUMNI RELATIONS IIT PATNA</h5>
+                        <hr>
+                        <h6>Contact Details:<br></h6>
+                        <p>Email Id: saar@iitp.ac.in<br>
+                            Phone No:77797997979<br>
+                            Address: IIT Patna Campus<br>
+                            District: Patna<br>
+                            Pin code:801110<br>
+                            State:Bihar, INDIA
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
         <footer class="py-5 bg-dark pad-4">
@@ -349,6 +373,32 @@ session_start();
       </div>
       <!-- /.container -->
     </footer>
+        <style>
+            .myclose{
+                font-size: 30px;
+                
+                position: absolute;
+                color: rgba(255,255,255,0.8);
+                top: 0;
+                right: 0;
+                padding: 5px 10px;
+            }
+            .myclose:hover{
+                cursor: pointer;
+                color: rgba(255,255,255,0.9);
+            }
+            
+            .contact-form{
+               background-color: rgba(0,0,0,0.9);
+               position: fixed;
+               top: 0;
+               left: 0;
+               z-index: 10000;
+               padding-top: 100px;
+               width: 100%;
+               height: 100%;
+            }
+        </style>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="asset/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -370,7 +420,7 @@ btn = document.getElementById('btn');
             ?>
         });};
 modal = document.getElementById('myModal');
-
+contactUsDom = document.getElementById('contact_form');
 close = document.getElementById('close');
 if(close){
 close.addEventListener('click', function(){
@@ -396,6 +446,9 @@ window.onclick = function(event){
     if(event.target == modal){
         modal.style.display="none";
     }
+    if(event.target ==contactUsDom ){
+        contactUsDom.style.display ="none";
+    }
 };
             <?php
 if(isset($_SESSION['loginfail']) && $_SESSION['loginfail'] == true)
@@ -414,7 +467,24 @@ if(isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true)
 function alertpass(){
     alert("Incorrect Credentials");
 }         
-            
+function show_pop(){
+    btn1.click();
+    return false;
+}
+function show_form(){
+    btn.click();
+};
+
+function contactUs(){
+    contactUsDom.style.display="block";
+    return false;
+}
+
+
+function close_modal(){
+    
+    contactUsDom.style.display="none";
+}
         </script>
         
     </body>
