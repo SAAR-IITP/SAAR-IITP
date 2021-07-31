fc1711aad56674a8949dcd6d66d098f1cc3910e0
Pratyush Kumar <40167899+StrixROX@users.noreply.github.com>
2021-07-31 22:37:45 +0530

    Updated Apps Script URL for yearbook form

--- yearbookForm.php
+++ yearbookForm.php
@@ -95,7 +95,7 @@
         <h2><span style="font-size: 30px;"><em>Registration Form</em></span></h2>
         <div class="main-agileinfo">
             <div class="agileits-top">
-                <iframe src="https://script.google.com/macros/s/AKfycbxbPIpIDWRhRl7xw5lz_8gnNx9QTjlQYtwGBiRt8B9QejbMuJz1FcFxudoJpbtvLLppyw/exec" frameborder="0" height="1414.58px" width="100%"></iframe>
+                <iframe src="https://script.google.com/macros/s/AKfycbzxorOjBJkKZ_b6uFzLYE0xSdnQGGuOEsW5mmZXdkG6q-0oNdjngko1k82k4yi-FLXL2Q/exec" frameborder="0" height="1414.58px" width="100%"></iframe>
                 <a href="./portal.php"><button style="font-size: .9em;
                     color: #fff;
                     background: #003C4D;
