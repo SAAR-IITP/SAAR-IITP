c403a2a5e9ee3e3c49d01c10826a6805aafe6b74
Pratyush Kumar <40167899+StrixROX@users.noreply.github.com>
2022-11-03 01:04:32 +0530

    Added links to Anusmriti Vol. 5 Issue 1 on homepage

--- index.php
+++ index.php
@@ -314,6 +314,7 @@
                         <div class="dropdown">
                             <button class="dropbtn">Read Anusmriti</button>
                             <div class="dropdown-content">
+				<a href="anusmriti/vol5/issue1.pdf" target="_blank">Volume 5, Issue 1</a>
                                 <a href="anusmriti/vol4/issue1.pdf" target="_blank">Volume 4, Issue 1</a>
                                 <a href="anusmriti/vol3/issue1.pdf" target="_blank">Volume 3, Issue 1</a>
                                 <a href="anusmriti/vol2/issue2.pdf" target="_blank">Volume 2, Issue 2</a>
